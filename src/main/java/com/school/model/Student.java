2a72bbecab71fc07ad64dd9712d4b19c8ca6e7ef
Mahmoud Osman <mahmoudosman.som@gmail.com>
2022-03-15 01:16:45 -0500

    configured h2 db, mysql db & added student model

---
+++ src/main/java/com/school/model/Student.java
@@ -0,0 +1,49 @@
+package com.school.model;
+
+public class Student {
+    private  Long id;
+    private String firstName;
+    private  String lastName;
+    private String email;
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
