c63331b42c1300913fb643f519e0b4e7210c15dc
Mahmoud Osman <mahmoudosman.som@gmail.com>
2022-03-20 15:58:14 -0500

    added lombok annotation on Student class

--- src/main/java/com/school/model/Student.java
+++ src/main/java/com/school/model/Student.java
@@ -1,9 +1,5 @@
 package com.school.model;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import java.util.Date;
