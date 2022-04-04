783ecaf251a00bd12c35130da87e7574cb57795f
Mahmoud Osman <mahmoudosman.som@gmail.com>
2022-04-03 23:57:52 -0500

    added swagger for api testing

--- src/main/java/com/school/OrmApplication.java
+++ src/main/java/com/school/OrmApplication.java
@@ -1,9 +1,13 @@
 package com.school;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Course API", version = "2.0", description = "Course Information API"))
+
 public class OrmApplication {
 
 	public static void main(String[] args) {
