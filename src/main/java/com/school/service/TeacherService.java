b36634a38c7b9adfd89ea6ced1cac1d58cfa9626
Mahmoud Osman <mahmoudosman.som@gmail.com>
2022-04-10 22:06:38 -0500

    added teacher service class

---
+++ src/main/java/com/school/service/TeacherService.java
@@ -0,0 +1,42 @@
+package com.school.service;
+import com.school.model.Teacher;
+import com.school.repository.TeacherRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TeacherService {
+
+    @Autowired
+    private TeacherRespository teacherRespository;
+
+
+    //Get method
+    public List<Teacher> getListOfTeacher(){
+        return teacherRespository.findAll();
+    }
+
+    //Post method
+    public Teacher saveTeacher(Teacher teacher){
+            return teacherRespository.save(teacher);
+    }
+
+    //Find by id method
+    public Teacher findTeacherById(Long teacherId){
+        return teacherRespository.findById(teacherId).get();
+    }
+
+
+    //Delete method
+    public void deleteTeacher(Long teacherId){
+         teacherRespository.deleteById(teacherId);
+    }
+
+
+
+
+}
+
+
